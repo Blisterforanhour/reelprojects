82b418b2b5b5d1593d5795ee01f03e50cbb14d8b
Blisterforanhour <1hourofbliss@gmail.com>
2025-06-29 16:51:17 +0200

    Fix ReelApps path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: '../../',
+  envDir: '../',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@reelapps/auth': path.resolve(__dirname, '../../ReelApps/packages/auth/src/index.ts'),
-      '@reelapps/ui': path.resolve(__dirname, '../../ReelApps/packages/ui/src/index.ts'),
-      '@reelapps-styles': path.resolve(__dirname, '../../ReelApps/src/styles'),
+      '@reelapps/auth': path.resolve(__dirname, '../ReelApps/packages/auth/src/index.ts'),
+      '@reelapps/ui': path.resolve(__dirname, '../ReelApps/packages/ui/src/index.ts'),
+      '@reelapps-styles': path.resolve(__dirname, '../ReelApps/src/styles'),
     },
   },
   server: {
